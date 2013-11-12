cae055284aeef76fe59f61d2caf236918fe59b09
Meno Abels <meno.abels@sinnerschrader.com>
2013-11-12 13:40:28 +0100

    update

--- db/datasource-postgresql/src/main/java/de/nextaudience/db/datasource/postgresql/Activator.java
+++ db/datasource-postgresql/src/main/java/de/nextaudience/db/datasource/postgresql/Activator.java
@@ -1,68 +1,40 @@
-package net.lr.db.datasource;
+package de.nextaudience.db.datasource.postgresql;
 
 import java.util.Dictionary;
 import java.util.Hashtable;
-
-import javax.sql.DataSource;
 
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
 import org.osgi.framework.Constants;
 import org.osgi.framework.ServiceRegistration;
-import org.osgi.service.cm.ConfigurationException;
-import org.osgi.service.cm.ManagedService;
-
-import com.mysql.jdbc.jdbc2.optional.MysqlDataSource;
+import org.osgi.service.cm.ManagedServiceFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class Activator implements BundleActivator {
-    
 
-    public final class DataSourceConfig implements ManagedService {
-        private final BundleContext context;
-        private ServiceRegistration serviceReg = null;
+    private static final Logger LOGGER = LoggerFactory.getLogger(Activator.class);
 
-        public DataSourceConfig(BundleContext context) {
-            this.context = context;
-        }
-
-        @SuppressWarnings("rawtypes")
-        private String getString(String key, Dictionary properties) {
-            Object value = properties.get(key);
-            return (value == null || !(value instanceof String)) ? "" : key;
-            
-        }
-
-        @SuppressWarnings("rawtypes")
-        @Override
-        public void updated(Dictionary properties) throws ConfigurationException {
-            if (serviceReg != null) {
-                try {
-                    serviceReg.unregister();
-                } catch (Exception e) {
-                }
-                serviceReg = null;
-            }
-            MysqlDataSource ds = new MysqlDataSource();
-            ds.setUrl(getString("url", properties));
-            ds.setUser(getString("user", properties));
-            ds.setPassword(getString("password", properties));
-
-            Dictionary<String, String> regProperties = new Hashtable<String, String>();
-            regProperties.put("osgi.jndi.service.name" , getString("name", properties));
-            serviceReg = context.registerService(DataSource.class, ds, regProperties);
-        }
-    }
-
+    private ServiceRegistration<ManagedServiceFactory> myReg;
+   
     @Override
-    public void start(final BundleContext context) throws Exception {
-        ManagedService managedService = new DataSourceConfig(context); 
+    public void start(BundleContext context) throws Exception {
+        LOGGER.info("starting factory...");
         Dictionary<String, String> properties = new Hashtable<String, String>();
-        properties.put(Constants.SERVICE_PID, "mysqldatasource");
-        context.registerService(ManagedService.class, managedService, properties);
+        properties.put(Constants.SERVICE_PID, "de.nextaudience.db.datasource.postgresql");
+        
+        myReg = context.registerService(ManagedServiceFactory.class, 
+                    new DataSourceFactory(context), 
+                    properties);
+        LOGGER.info("registered as ManagedServiceFactory");
     }
 
     @Override
     public void stop(BundleContext context) throws Exception {
+        if (myReg != null) {
+            myReg.unregister();
+        } else {
+            LOGGER.info("my service registration as already null " + "(although it shouldn't)!");
+        }
     }
-
 }
---
+++ db/datasource-postgresql/src/main/java/de/nextaudience/db/datasource/postgresql/DataSourceAndServiceRegistration.java
@@ -0,0 +1,54 @@
+package de.nextaudience.db.datasource.postgresql;
+
+import java.sql.SQLException;
+import java.util.Dictionary;
+import java.util.Hashtable;
+
+import javax.sql.DataSource;
+
+import org.apache.commons.dbcp.BasicDataSource;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceRegistration;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+public class DataSourceAndServiceRegistration {
+    
+    private static final Logger LOGGER = LoggerFactory.getLogger(DataSourceAndServiceRegistration.class);
+    
+    final public BasicDataSource dataSource = new BasicDataSource();
+    final public String applicationName;
+    final public ServiceRegistration<DataSource> serviceRegistration;
+    
+    private static String getString(String key, Dictionary<?, ?> properties) {
+        Object value = properties.get(key);
+        return (value == null || !(value instanceof String)) ? "" : (String)value;   
+    }
+    
+    private static String getApplicationName(BundleContext ctx, Dictionary<?, ?> properties) {
+        return "DS-"+ctx.getBundle().getBundleId()+"-"+getString("pid", properties)+"-"+getString("name", properties);
+    }
+
+
+    public DataSourceAndServiceRegistration(Dictionary<?, ?> connectionProp, BundleContext ctx) {
+        dataSource.setDriverClassName("org.postgresql.ds.PGSimpleDataSource");           
+        applicationName = getApplicationName(ctx, connectionProp);    
+        dataSource.setUsername(getString("user", connectionProp));
+        dataSource.setPassword(getString("password", connectionProp));
+        dataSource.setUrl(getString("url", connectionProp));
+        
+        final Dictionary<String, String> regProperties = new Hashtable<String, String>();
+        regProperties.put("osgi.jndi.service.name" , DataSourceAndServiceRegistration.getString("name", connectionProp)); 
+
+        serviceRegistration = ctx.registerService(DataSource.class, dataSource, regProperties);
+    }
+    public void unregister() {
+        try {
+            dataSource.close();
+        } catch (SQLException e) {
+            LOGGER.error("dataSource.close failed", e);
+        }
+        serviceRegistration.unregister();
+    }
+}
