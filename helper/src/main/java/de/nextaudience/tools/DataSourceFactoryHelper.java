53bc82e4e3fa4402f88be53328c1ac34ab4e686a
Klaus Schröder <klaus.schroeder@next-audience.com>
2013-12-11 18:17:15 +0100

    Added IPOJOInstanceHelper, use IPOJO for EshopManager

---
+++ helper/src/main/java/de/nextaudience/tools/DataSourceFactoryHelper.java
@@ -0,0 +1,47 @@
+package de.nextaudience.tools;
+
+import javax.sql.DataSource;
+
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceReference;
+
+import de.nextaudience.db.datasource.dynamic.DynamicDataSourceFactory;
+
+
+public class DataSourceFactoryHelper {
+
+    private DynamicDataSourceFactory dataSourceFactory;
+    private String url;
+    private String pid;
+    private final OSGiHelper osgiHelper;
+
+    public DataSourceFactoryHelper(final BundleContext bundleContext) {
+        this.osgiHelper = new OSGiHelper(bundleContext);
+    }
+
+    public void create(final DynamicDataSourceFactory dataSourceFactory, final String pid, final String name, final String url, final String user,
+            final String password) throws Exception {
+        if (this.url != null) {
+            throw new RuntimeException("Datasource must be disposed before reusing a DataSourceFactoryHelper.");
+        }
+        @SuppressWarnings("rawtypes")
+        ServiceReference sr = this.osgiHelper.getServiceReferenceByPID(DataSource.class, pid);
+        if (sr == null) {
+            this.dataSourceFactory = dataSourceFactory;
+            this.pid = pid;
+            this.url = url;
+            dataSourceFactory.createDataSource(pid, name, url, user, password);
+        } else {
+            this.dataSourceFactory = null;
+            this.pid = null;
+            this.url = null;
+        }
+    }
+
+    public void dispose() {
+        if (this.url != null) {
+            this.dataSourceFactory.deleteDataSource(this.pid, this.url);
+        }
+    }
+
+}
---
+++ helper/src/main/java/de/nextaudience/tools/IPOJOInstanceHelper.java
@@ -0,0 +1,89 @@
+package de.nextaudience.tools;
+
+import java.util.Dictionary;
+
+import org.apache.felix.ipojo.ComponentInstance;
+import org.osgi.framework.BundleContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class IPOJOInstanceHelper {
+
+    private static final Logger LOG = LoggerFactory.getLogger(IPOJOInstanceHelper.class);
+    private final OSGiHelper osgiHelper;
+    private final IPOJOHelper ipojoHelper;
+
+
+    public IPOJOInstanceHelper(final BundleContext bundleContext) {
+        this.ipojoHelper = new IPOJOHelper(bundleContext);
+        this.osgiHelper = new OSGiHelper(bundleContext);
+    }
+
+    /**
+     * Create an iPojo instance for a given class and a given instance name using the given properties.
+     * 
+     * @param instanceClass
+     *            class of the instance to be instantiated
+     * @param instanceName
+     *            instance name
+     * @param props
+     *            properties to be used to instantiate the object
+     * @returns {@link InstanceHolder} object holding the instance just created and the IPOJO description object of class
+     *          {@link ComponentInstance}
+     */
+    public <I, T> InstanceHolder<I> create(final Class<I> interfaceClass, final String instanceClassName,
+            final String instanceName, final Dictionary<String, Object> props) {
+        if (instanceName != null) {
+            props.put("instance.name", instanceName);
+        }
+        // Create, register and start an instance of class instanceClass
+        ComponentInstance ci = this.ipojoHelper.createComponentInstance(instanceClassName, props);
+
+        // get the just created instance object from the OSGI registry and return it
+        final I instance = this.osgiHelper.getServiceObject(interfaceClass, "(instance.name=" + ci.getInstanceName() + ")");
+        if (instance != null && interfaceClass.isInstance(instance)) {
+            LOG.info("Created instance of class '{}' with instance name '{}'successfully.", instanceClassName,
+                    ci.getInstanceName());
+        } else {
+            LOG.info("Creation of instance of class '{}' failed.", instanceClassName);
+        }
+        return new InstanceHolder<I>(instance, ci);
+    }
+
+    /**
+     * Disposes the iPojo instance.
+     */
+    public <T> void dispose(InstanceHolder<T> holder) {
+        if (holder != null ) {
+            holder.dispose();
+        }
+    }
+
+
+    public class InstanceHolder<T> {
+
+        private final T instance;
+        private ComponentInstance componentInstance;
+
+
+        public InstanceHolder(final T instance, final ComponentInstance ci) {
+            this.instance = instance;
+            this.componentInstance = ci;
+        }
+
+        public T getInstance() {
+            return this.instance;
+        }
+
+        public ComponentInstance getComponentInstance() {
+            return this.componentInstance;
+        }
+
+        public void dispose() {
+            if (this.componentInstance != null) {
+                this.componentInstance.dispose();
+                this.componentInstance = null;
+            }
+        }
+    }
+}
