91ac64524cb147c3dd34687eee0b1420350c5410
Klaus Schröder <klaus.schroeder@next-audience.com>
2014-01-30 19:41:01 +0100

    Extended tools-datasource-base to allow create and drop of databases

--- datasource/base/src/main/java/de/nextaudience/db/datasource/base/DataSourceFactory.java
+++ datasource/base/src/main/java/de/nextaudience/db/datasource/base/DataSourceFactory.java
@@ -1,12 +1,9 @@
 package de.nextaudience.db.datasource.base;
 
-import de.nextaudience.db.datasource.DriverFactory;
-import de.nextaudience.tools.IPOJOInstanceHelper;
 import java.sql.Connection;
 import java.sql.Driver;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.Dictionary;
 import java.util.Enumeration;
 import java.util.HashMap;
@@ -15,7 +12,9 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.Properties;
+
 import javax.sql.DataSource;
+
 import org.apache.commons.dbcp.ConnectionFactory;
 import org.apache.commons.dbcp.PoolableConnectionFactory;
 import org.apache.commons.dbcp.PoolingDataSource;
@@ -34,6 +33,9 @@ import org.osgi.framework.ServiceReference;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import de.nextaudience.db.datasource.DriverFactory;
+import de.nextaudience.tools.IPOJOInstanceHelper;
+
 @Component
 @Instantiate
 @Provides
@@ -45,17 +47,20 @@ public class DataSourceFactory implements de.nextaudience.db.datasource.DataSour
 
     private final List<DriverAndServiceReference> drivers = new LinkedList<DriverAndServiceReference>();
 
-    private IPOJOInstanceHelper ipojoInstanceHelper;
+    private final IPOJOInstanceHelper ipojoInstanceHelper;
 
 
+    /** {@inheritDoc} */
     @Override
-    public String createDataSource(Dictionary<String, String> props) {
+    public String createDataSource(final Dictionary<String, String> props) {
         final String pid = props.get(Constants.SERVICE_PID);
         if (pid == null) {
             LOGGER.error("DataSourceFactory:createDataSource: property '" + Constants.SERVICE_PID + "' is not set");
+            return null;
         }
         if (props.get("driver") == null) {
             LOGGER.error("DataSourceFactory:createDataSource: property 'driver' is not set");
+            return null;
         }
 
         final DataSourceParams dataSourceParams = new DataSourceParams(props);
@@ -69,7 +74,7 @@ public class DataSourceFactory implements de.nextaudience.db.datasource.DataSour
             return null;
         }
         LOGGER.info("DriverAndServiceReference:{}=>{}", driverName, dasr);
-        for (String key : dasr.serviceReference.getPropertyKeys()) {
+        for (final String key : dasr.serviceReference.getPropertyKeys()) {
             LOGGER.info("ServiceReference:driver.{}=>{}", key, dasr.serviceReference.getProperty(key));
             props.put("driver." + key, dasr.serviceReference.getProperty(key).toString());
         }
@@ -79,7 +84,7 @@ public class DataSourceFactory implements de.nextaudience.db.datasource.DataSour
         props.put("osgi.jndi.service.name", getString("name", props));
         props.put(Constants.SERVICE_PID, pid);
         props.put("instance.name", getString("name", props));
-        Enumeration<String> keys = props.keys();
+        final Enumeration<String> keys = props.keys();
         final Dictionary<String, Object> soProps = new Hashtable<String, Object>();
         while (keys.hasMoreElements()) {
             final String key = keys.nextElement();
@@ -87,88 +92,109 @@ public class DataSourceFactory implements de.nextaudience.db.datasource.DataSour
             LOGGER.info("{}={}", key, props.get(key));
         }
         soProps.put("dataSourceParams", dataSourceParams);
-        dataSourceParams.instanceHolder = ipojoInstanceHelper.create(DataSource.class, IPojoPoolingDataSource.class.getName(), pid,
+        dataSourceParams.instanceHolder = this.ipojoInstanceHelper.create(DataSource.class, IPojoPoolingDataSource.class.getName(), pid,
                 soProps);
 
         return pid;
     }
 
+    /** {@inheritDoc} */
     @Override
-    public void deleteDataSource(String pid) {
+    public void deleteDataSource(final String pid) {
         final DataSourceParams dsp = pid2dataSourceParams.get(pid);
         if (dsp != null) {
             LOGGER.info("DataSourceFactory:deleteDataSource:{}", pid);
             dsp.instanceHolder.dispose();
             try {
                 dsp.poolableConnectionFactory.getPool().close();
-            } catch (Exception ex) {
+            } catch (final Exception ex) {
                 LOGGER.error("deleteDataSource:{} {}", pid, ex);
             }
             pid2dataSourceParams.remove(pid);
         }
     }
 
-	private DataSource findDataSourceByName(String dname) {
-		for(DataSourceParams dsp : pid2dataSourceParams.values()) {
-			if (dsp.props.get("dataBaseName").equals(dname)) {
-				return dsp.poolingDataSource;
-			}
-		}
-		return null;
-	}
+    /** {@inheritDoc} */
+    @Override
+    public String createDatabaseFromName(final Dictionary<String, String> props) throws SQLException {
+        final DataSource systemds = getSystemDS(props);
+        if (systemds == null) {
+            LOGGER.error("Could not drop database {}. No system datasource found to drop database for PID.", props.get(PROP_CREATE_DATABASE_NAME));
+            return null;
+        }
+        runStmt(systemds, "CREATE DATABASE " + props.get(PROP_CREATE_DATABASE_NAME), props.get(PROP_CREATE_DATABASE_NAME));
+        LOGGER.info("Successfully created database for PID {}.", props.get(PROP_CREATE_DATABASE_NAME));
+        return createDataSource(props);
+    }
 
-	private DataSource getSystemDS(Dictionary<String, String> props) {
-		final String databaseName = props.get("dataBaseName");
-		final String systemDataSourceName = props.get("createDatabaseDataSourceName");
-		if (databaseName == null || databaseName.isEmpty() ||
-			systemDataSourceName == null || systemDataSourceName.isEmpty()) {
-			LOGGER.error("props must contain key:dataBaseName or key:createDatabaseDataSourceName");
-			return null;
-		}
-		final DataSource systemds = findDataSourceByName(systemDataSourceName);
-		if (systemds == null) {
-			LOGGER.error("can not find datasource from name="+databaseName);
-			return null;
-		}
-		return systemds;
-	}
+    /** {@inheritDoc} */
+    @Override
+    public void dropDatabase(final Dictionary<String, String> props) throws SQLException {
+        final String pid = props.get(Constants.SERVICE_PID);
+        DataSourceParams param = pid2dataSourceParams.get(pid);
+        if (param == null) {
+            // for safety create a database instance first before dropping it
+            if (createDataSource(props) == null) {
+                LOGGER.error("Could not drop database {}. The datasource could not be created.", pid);
+                return;
+            }
+        }
+        // now the datasource should be in the pid map
+        param = pid2dataSourceParams.get(pid);
+        if (param != null) {
+            // datasource for PID found, delete the datasource instance
+            deleteDataSource(pid);
+        } else {
+            LOGGER.error("Could not drop database {}. The datasource could not be created.", pid);
+            return;
+        }
 
-	private void runStmt(DataSource ds, String sql, String ...params) throws SQLException {
-		final Connection connection = ds.getConnection();
-		try {
-			final PreparedStatement ps = connection.prepareStatement(sql);
-			int idx = 0;
-			for (String param : params) {
-				ps.setString(idx++, param);
-			}
-			final ResultSet rs = ps.executeQuery();
-			LOGGER.error("RESULT Set:"+rs);
-			ps.close();
-		} finally {
-			connection.close();
-		}
-	}
+        final DataSource systemds = getSystemDS(props);
+        if (systemds == null) {
+            LOGGER.error("Could not drop database {}. No system datasource found to drop database for PID.", pid);
+            return;
+        }
+        runStmt(systemds, "DROP DATABASE " + props.get(PROP_CREATE_DATABASE_NAME), props.get(PROP_CREATE_DATABASE_NAME));
+        LOGGER.info("Successfully dropped database for PID {}.", pid);
+    }
 
-	public String createdatabaseFromName(Dictionary<String, String> props) throws SQLException {
-		final DataSource systemds = getSystemDS(props);
-		if (systemds == null) {
-			return null;
-		}
-		runStmt(systemds, "create database ?", props.get("dataBaseName"));
-		return createDataSource(props);
-	}
+    private DataSource findDataSourceByName(final String dname) {
+        for (final DataSourceParams dsp : pid2dataSourceParams.values()) {
+            if (dname != null && dname.equals(dsp.props.get("name"))) {
+                return dsp.poolingDataSource;
+            }
+        }
+        return null;
+    }
 
-	public void dropdatabase(String pid) throws SQLException {
-		Dictionary<String, String> props = pid2dataSourceParams.get(pid).props;
-		final DataSource systemds = getSystemDS(props);
-		if (systemds == null) {
-			return;
-		}
-		deleteDataSource(pid);
-		runStmt(systemds, "drop database ?", props.get("dataBaseName"));	
-	}
+    private DataSource getSystemDS(final Dictionary<String, String> props) {
+        final String databaseName = props.get(PROP_CREATE_DATABASE_NAME);
+        final String systemDataSourceName = props.get(PROP_CREATE_DATABASE_DATASOURCE_NAME);
+        if (databaseName == null || databaseName.isEmpty() || systemDataSourceName == null || systemDataSourceName.isEmpty()) {
+            LOGGER.error("Properties must contain key:{} and key:{}", PROP_CREATE_DATABASE_NAME, PROP_CREATE_DATABASE_DATASOURCE_NAME);
+            return null;
+        }
+        final DataSource systemds = findDataSourceByName(systemDataSourceName);
+        if (systemds == null) {
+            LOGGER.error("Can't find datasource from name {}", databaseName);
+            return null;
+        }
+        return systemds;
+    }
 
-    public DataSourceFactory(BundleContext context) {
+    private void runStmt(final DataSource ds, final String sql, final String... params) throws SQLException {
+        final Connection connection = ds.getConnection();
+        try {
+            connection.setAutoCommit(true);
+            final Statement ps = connection.createStatement();
+            ps.execute(sql);
+            ps.close();
+        } finally {
+            connection.close();
+        }
+    }
+
+    public DataSourceFactory(final BundleContext context) {
         this.ipojoInstanceHelper = new IPOJOInstanceHelper(context);
     }
 
@@ -183,15 +209,13 @@ public class DataSourceFactory implements de.nextaudience.db.datasource.DataSour
     }
 
     @Bind(optional = true, aggregate = true)
-    public void addDriver(DriverFactory driver, ServiceReference<DriverFactory> sr) {
-        final String driverSignature = String.format("%s-%s-%s",
-            driver.getClass().getCanonicalName(),
-            sr.getProperty("name"),
-            sr.getProperty("fullName"));
+    public void addDriver(final DriverFactory driver, final ServiceReference<DriverFactory> sr) {
+        final String driverSignature = String.format("%s-%s-%s", driver.getClass().getCanonicalName(), sr.getProperty("name"),
+                sr.getProperty("fullName"));
         LOGGER.info("DataSourceFactory:addDriver:{}", driver.getClass().getCanonicalName());
-        drivers.add(new DriverAndServiceReference(driver, sr));
+        this.drivers.add(new DriverAndServiceReference(driver, sr));
         // Replay Connection
-        for (Map.Entry<String, DataSourceParams> entry : pid2dataSourceParams.entrySet()) {
+        for (final Map.Entry<String, DataSourceParams> entry : pid2dataSourceParams.entrySet()) {
             if (isDriver(entry.getValue().props.get("driver"), sr)) {
                 if (entry.getValue().serviceRegistration != null) {
                     LOGGER.error("addDriver has an registered DataSources:{}", driverSignature);
@@ -202,33 +226,33 @@ public class DataSourceFactory implements de.nextaudience.db.datasource.DataSour
         }
     }
 
-    private boolean isDriver(String name, ServiceReference sr) {
-        final String srName = (String)sr.getProperty("name");
+    @SuppressWarnings("rawtypes")
+    private boolean isDriver(final String name, final ServiceReference sr) {
+        final String srName = (String) sr.getProperty("name");
         if (srName != null && srName.equals(name)) {
             return true;
         }
-        final String srFullName = (String)sr.getProperty("fullName");
+        final String srFullName = (String) sr.getProperty("fullName");
         if (srFullName != null && srFullName.equals(name)) {
             return true;
         }
         return false;
     }
 
-    private boolean isDriver(Dictionary<String, String> prop, ServiceReference sr2) {
-        return isDriver(prop.get("name"), sr2)
-                || isDriver(prop.get("fullName"), sr2);
+    @SuppressWarnings({ "unused", "rawtypes" })
+    private boolean isDriver(final Dictionary<String, String> prop, final ServiceReference sr2) {
+        return isDriver(prop.get("name"), sr2) || isDriver(prop.get("fullName"), sr2);
     }
 
-    private boolean isDriver(ServiceReference sr1, ServiceReference sr2) {
-        return isDriver(sr1.getProperty("name").toString(), sr2)
-                || isDriver(sr1.getProperty("fullName").toString(), sr2);
+    @SuppressWarnings("rawtypes")
+    private boolean isDriver(final ServiceReference sr1, final ServiceReference sr2) {
+        return isDriver(sr1.getProperty("name").toString(), sr2) || isDriver(sr1.getProperty("fullName").toString(), sr2);
     }
 
-    private DriverAndServiceReference findByName(String name) {
-        for (DriverAndServiceReference driver : drivers) {
-            LOGGER.info("findByName:{}=={}||{}", name,
-                driver.serviceReference.getProperty("name"),
-                driver.serviceReference.getProperty("fullName"));
+    private DriverAndServiceReference findByName(final String name) {
+        for (final DriverAndServiceReference driver : this.drivers) {
+            LOGGER.info("findByName:{}=={}||{}", name, driver.serviceReference.getProperty("name"),
+                    driver.serviceReference.getProperty("fullName"));
             if (isDriver(name, driver.serviceReference)) {
                 return driver;
             }
@@ -237,25 +261,25 @@ public class DataSourceFactory implements de.nextaudience.db.datasource.DataSour
     }
 
     @Unbind(optional = true, aggregate = true)
-    public void delDriver(DriverFactory driver, ServiceReference<DriverFactory> sr) {
+    public void delDriver(final DriverFactory driver, final ServiceReference<DriverFactory> sr) {
         try {
             final String name = sr.getProperty("name").toString();
             final String fullName = sr.getProperty("fullName").toString();
             LOGGER.info("DataSourceFactory:delDriver:{}", driver.getClass().getCanonicalName());
-            drivers.remove(findByName(name));
-            drivers.remove(findByName(fullName));
+            this.drivers.remove(findByName(name));
+            this.drivers.remove(findByName(fullName));
             final List<DataSourceParams> toRenew = new LinkedList<DataSourceParams>();
-            for (Map.Entry<String, DataSourceParams> entry: pid2dataSourceParams.entrySet()) {
+            for (final Map.Entry<String, DataSourceParams> entry : pid2dataSourceParams.entrySet()) {
                 if (isDriver(sr, entry.getValue().serviceRegistration.getReference())) {
                     toRenew.add(entry.getValue());
                 }
             }
-            for (DataSourceParams dsp : toRenew) {
+            for (final DataSourceParams dsp : toRenew) {
                 deleteDataSource(dsp.props.get(Constants.SERVICE_PID));
                 dsp.serviceRegistration = null;
                 pid2dataSourceParams.put(dsp.props.get(Constants.SERVICE_PID), dsp);
             }
-        } catch (Exception e) {
+        } catch (final Exception e) {
             LOGGER.error("Error while deleting JDBC driver: ", e.getMessage());
         }
     }
@@ -265,20 +289,20 @@ public class DataSourceFactory implements de.nextaudience.db.datasource.DataSour
         return (value instanceof String) ? (String) value : null;
     }
 
-    public static DataSourceParams create(final Driver driver, Dictionary<String, String> prop, DataSourceParams dsp) {
+    public static DataSourceParams create(final Driver driver, final Dictionary<String, String> prop, final DataSourceParams dsp) {
         final Properties driverProp = new Properties();
         final Enumeration<String> keys = prop.keys();
         while (keys.hasMoreElements()) {
-            String key = keys.nextElement();
+            final String key = keys.nextElement();
             driverProp.setProperty(key, prop.get(key));
         }
         final String uri = getString("url", prop);
-        int jdbcIdx = uri.indexOf(':');
+        final int jdbcIdx = uri.indexOf(':');
         if (jdbcIdx <= 0) {
             LOGGER.error("url not parsable:{}", uri);
             return null;
         }
-        int uriIdx = uri.indexOf(':', jdbcIdx+1);
+        final int uriIdx = uri.indexOf(':', jdbcIdx + 1);
         if (uriIdx <= 0) {
             LOGGER.error("url not parsable:{}", uri);
             return null;
@@ -289,9 +313,7 @@ public class DataSourceFactory implements de.nextaudience.db.datasource.DataSour
             @Override
             public Connection createConnection() throws SQLException {
                 final Connection connection = driver.connect(uri, driverProp);
-                LOGGER.info("ConnectionFactory:createConnection:{}:{}:{}",
-                    driver.getClass().getName(),
-                    uri, connection);
+                LOGGER.info("ConnectionFactory:createConnection:{}:{}:{}", driver.getClass().getName(), uri, connection);
                 return connection;
             }
         };
@@ -299,8 +321,8 @@ public class DataSourceFactory implements de.nextaudience.db.datasource.DataSour
         final GenericObjectPool connectionPool = new GenericObjectPool();
         connectionPool.setMaxActive(30);
 
-        GenericKeyedObjectPool.Config stmtConfig = new GenericKeyedObjectPool.Config();
-        //stmtConfig.lifo = true;
+        final GenericKeyedObjectPool.Config stmtConfig = new GenericKeyedObjectPool.Config();
+        // stmtConfig.lifo = true;
         stmtConfig.maxActive = 1024;
         stmtConfig.maxIdle = 64;
         stmtConfig.maxTotal = -1;
@@ -309,13 +331,9 @@ public class DataSourceFactory implements de.nextaudience.db.datasource.DataSour
 
         // final KeyedObjectPoolFactory stmtPoolFactory = new GenericKeyedObjectPoolFactory(null, stmtConfig);
 
-        dsp.poolableConnectionFactory = new PoolableConnectionFactory(
-            connectionFactory,
-            connectionPool,
-            null,
-            null, //getString("driver.sql.validation.query", prop),
-            false,
-            false);
+        dsp.poolableConnectionFactory = new PoolableConnectionFactory(connectionFactory, connectionPool, null, null, // getString("driver.sql.validation.query",
+                // prop),
+                false, false);
         dsp.poolingDataSource = new PoolingDataSource(dsp.poolableConnectionFactory.getPool());
 
         return dsp;
